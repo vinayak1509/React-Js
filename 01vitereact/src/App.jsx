32955ec5dd6f6a7d0e9a8d26775016ffde19f945
Vinayak <vinayak.77thakur@gmail.com>
2026-05-21 11:41:10 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,6 +2,11 @@ import Chai from "./chai"
 
 
 function App() {
+  const username  = "Vinayak"
+
+  // We can also inject some variables inside {}
+
+
 
   return (
     // <h1>Chai aur react with Vite | Vinayak</h1>
@@ -10,14 +15,22 @@ function App() {
     // <h1></h1>
     // <p></p>    // this is the case of error : in jsx, you can export max of 1 element only...
     
+    // the "{username}" is called evaluated expression... this is a JS
+    // but u can't write if or for loops, this should only contain the final evaluated expression
+
+    
     <>
     <Chai/>
-    <h1></h1>
-    <p></p>
+
+    <h1>hello {username}</h1> 
+
+    
     </>  // now it is okay .. you can export muktiple elements inside a FRAGMENT
   )             
 }
 // funcitons' name which are the components should start with upper case letter.
 // if any html is being returned from the file, name it .jsx instead of .js
+// react in browser :- ReactDOM
+// React in mobile :- React native 
 export default App
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// brought from the custom react
+// const reactElement = {
+//     type: 'a',
+//     props : {
+//         href: 'https://google.com',
+//         target : '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
 )
+// after all the tree formation, variable injections are done
+const anotherUser = "chai aur react"
+
+// We are creating new react element but this time as per the original react
+const reactElement = React.createElement(
+  // The React has a fucntion called " createElement" which 
+    // expects some parameters : first is the type of tag
+    // then second is an object which has href and target
+    // and last is the text
+
+
+
+  'a',
+  {href: "https://google.com", target: '_blank'},
+  'click me to visit google',
+  anotherUser
+  // you can't write if else for loop etc... as at the end of the day , this is
+  // an object
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    
+   // <MyApp/>
+   //<App/>
+    //  anotherElement
+
+    // Following react element is the one we created later on using the react syntax,
+    // that's why it runs without any problem, earlier the reactElement 
+    // was not able to run because that was not meeting the requirement of the 
+    // render function..
+    // the render function expects some parameters in certain syntax, so
+    // it should get the parameters as per its requirement
+
+    reactElement
+     
+)
+/* 
+MyApp and App are just functions that's they can be run as a simple function also : 
+MyApp() instead of HTML style but we won't use in that manner 
+
+The stuffs that are written inside App or MyApp, are parsed into object style first as the customReact object shown
+*/
+
+
+
