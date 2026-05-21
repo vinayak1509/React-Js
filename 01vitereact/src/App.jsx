3be3b05e24ee7e380bdc284f2343e8c751d4ecaf
Vinayak <vinayak.77thakur@gmail.com>
2026-05-21 09:33:50 +0530

    react flow and structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,23 @@
+import Chai from "./chai"
 
 
 function App() {
 
   return (
-    <h1>Chai aur react with Vite | Vinayak</h1>
-  )
+    // <h1>Chai aur react with Vite | Vinayak</h1>
+    
+    // <Chai/>
+    // <h1></h1>
+    // <p></p>    // this is the case of error : in jsx, you can export max of 1 element only...
+    
+    <>
+    <Chai/>
+    <h1></h1>
+    <p></p>
+    </>  // now it is okay .. you can export muktiple elements inside a FRAGMENT
+  )             
 }
-
+// funcitons' name which are the components should start with upper case letter.
+// if any html is being returned from the file, name it .jsx instead of .js
 export default App
+
